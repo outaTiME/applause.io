b5d495cc56e73e0143f1985e5431ce9b0bd223ce
outaTiME <afalduto@gmail.com>
2015-09-21 21:03:14 -0300

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,335 @@
+
+/*
+ The MIT License (MIT)
+
+ Copyright (c) 2015 outaTiME
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  // Automatically load required grunt tasks
+  require('jit-grunt')(grunt, {
+    useminPrepare: 'grunt-usemin',
+    usebanner: 'grunt-banner'
+  });
+
+  // Configurable paths
+  var config = {
+    app: 'app',
+    dist: 'dist',
+    pkg: grunt.file.readJSON('package.json'),
+    author: '<%= config.pkg.author.name %>',
+    version: '<%= config.pkg.version %>',
+    copyrightNotice: 'Copyright © <%= grunt.template.today("yyyy") %> <%= config.author %>. All rights reserved.',
+    banner: '/*!\n' +
+      ' *              _\n' +
+      ' *  ___ ___ ___| |___ _ _ ___ ___\n' +
+      ' * | .\'| . | . | | .\'| | |_ -| -_|\n' +
+      ' * |__,|  _|  _|_|__,|___|___|___| v.<%= config.version %>\n' +
+      ' *     |_| |_|\n' +
+      ' *\n' +
+      ' * <%= config.copyrightNotice %>\n' +
+      ' */\n'
+  };
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    // Project settings
+    config: config,
+
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      js: {
+        files: ['<%= config.app %>/scripts/{,*/}*.js'],
+        tasks: ['eslint', 'browserify']
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      styles: {
+        files: ['<%= config.app %>/styles/{,*/}*.css'],
+        tasks: ['newer:copy:styles', 'postcss']
+      }
+    },
+
+    browserSync: {
+      options: {
+        notify: false,
+        background: true,
+        open: false,
+        watchOptions: {
+          ignored: ''
+        }
+      },
+      livereload: {
+        options: {
+          files: [
+            '<%= config.app %>/{,*/}*.html',
+            '.tmp/styles/{,*/}*.css',
+            '.tmp/scripts/{,*/}*.js'
+          ],
+          port: 9000,
+          server: {
+            baseDir: ['.tmp', config.app],
+            routes: {
+              '/bower_components': './bower_components'
+            }
+          }
+        }
+      },
+      dist: {
+        options: {
+          background: false,
+          server: '<%= config.dist %>'
+        }
+      }
+    },
+
+    // Empties folders to start fresh
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= config.dist %>/*',
+            '!<%= config.dist %>/.git*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
+
+    // Make sure code styles are up to par and there are no obvious mistakes
+    eslint: {
+      target: [
+        'Gruntfile.js',
+        '<%= config.app %>/scripts/{,*/}*.js',
+        '!<%= config.app %>/scripts/vendor/*'
+      ]
+    },
+
+    browserify: {
+      dist: {
+        src: ['<%= config.app %>/scripts/**/*.js'],
+        dest: '.tmp/scripts/main.js'
+      }
+    },
+
+    postcss: {
+      options: {
+        map: true,
+        processors: [
+          // Add vendor prefixed styles
+          require('autoprefixer')({
+            browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']
+          })
+        ]
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/styles/',
+          src: '{,*/}*.css',
+          dest: '.tmp/styles/'
+        }]
+      }
+    },
+
+    // Renames files for browser caching purposes
+    filerev: {
+      dist: {
+        src: [
+          '<%= config.dist %>/scripts/{,*/}*.js',
+          '<%= config.dist %>/styles/{,*/}*.css',
+          '<%= config.dist %>/styles/fonts/{,*/}*.*',
+          '<%= config.dist %>/*.{ico,png}'
+        ]
+      }
+    },
+
+    // Reads HTML for usemin blocks to enable smart builds that automatically
+    // concat, minify and revision files. Creates configurations in memory so
+    // additional tasks can operate on them
+    useminPrepare: {
+      options: {
+        dest: '<%= config.dist %>'
+      },
+      html: '<%= config.app %>/index.html'
+    },
+
+    // Performs rewrites based on rev and the useminPrepare configuration
+    usemin: {
+      options: {
+        assetsDirs: [
+          '<%= config.dist %>',
+          '<%= config.dist %>/styles'
+        ]
+      },
+      html: ['<%= config.dist %>/{,*/}*.html'],
+      css: ['<%= config.dist %>/styles/{,*/}*.css']
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+          removeAttributeQuotes: true,
+          removeCommentsFromCDATA: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true,
+          // true would impact styles with attribute selectors
+          removeRedundantAttributes: false,
+          useShortDoctype: true,
+          // custom
+          removeComments: true,
+          minifyJS: true
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= config.dist %>',
+          src: '{,*/}*.html',
+          dest: '<%= config.dist %>'
+        }]
+      }
+    },
+
+    // Copies remaining files to places other tasks can use
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= config.app %>',
+          dest: '<%= config.dist %>',
+          src: [
+            '*.{ico,png,txt}',
+            '{,*/}*.html',
+            'styles/fonts/{,*/}*.*'
+          ]
+        }]
+      },
+      styles: {
+        expand: true,
+        dot: true,
+        cwd: '<%= config.app %>/styles',
+        dest: '.tmp/styles/',
+        src: '{,*/}*.css'
+      }
+    },
+
+    // Run some tasks in parallel to speed up build process
+    concurrent: {
+      server: [
+        'copy:styles'
+      ],
+      dist: [
+        'copy:styles'
+      ]
+    },
+
+    // custom
+
+    usebanner: {
+      dist: {
+        options: {
+          banner: '<%= config.banner %>',
+        },
+        files: {
+          src: [
+            '<%= config.dist %>/**/*.js',
+            '<%= config.dist %>/**/*.css'
+          ]
+        }
+      }
+    },
+
+    buildcontrol: {
+      dist: {
+        options: {
+          remote: 'git@github.com:outaTiME/applause.io.git',
+          branch: 'gh-pages',
+          commit: true,
+          push: true
+        }
+      }
+    }
+
+  });
+
+
+  grunt.registerTask('serve', 'start the server and preview your app', function (target) {
+
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'browserSync:dist']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'concurrent:server',
+      'browserify',
+      'postcss',
+      'browserSync:livereload',
+      'watch'
+    ]);
+  });
+
+  grunt.registerTask('server', function (target) {
+    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+    grunt.task.run([target ? ('serve:' + target) : 'serve']);
+  });
+
+  grunt.registerTask('build', [
+    'clean:dist',
+    'useminPrepare',
+    'concurrent:dist',
+    'browserify',
+    'postcss',
+    'concat',
+    'cssmin',
+    'uglify',
+    'copy:dist',
+    'filerev',
+    'usemin',
+    'htmlmin',
+    'usebanner'
+  ]);
+
+  grunt.registerTask('default', [
+    'newer:eslint',
+    'build'
+  ]);
+
+  // Custom
+  grunt.registerTask('deploy', [
+    'default',
+    'buildcontrol'
+  ]);
+
+};
---
+++ app/scripts/main.js
@@ -0,0 +1,173 @@
+
+/*
+ The MIT License (MIT)
+
+ Copyright (c) 2015 outaTiME
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+'use strict';
+
+/*global CodeMirror*/
+/*eslint-disable no-console*/
+
+// browserify
+
+var Applause = require('applause');
+
+// defaults
+
+// TODO: took from file
+var defaultOptions = [
+  '{',
+  '  "patterns": [',
+  '    {',
+  '      "match": /(\\w+)\\s(\\w+)/,',
+  '      "replacement": "$2, $1"',
+  '    }',
+  '  ]',
+  '}'
+].join('\n');
+
+var defaultText = 'John Smith';
+
+// editors
+
+var optionsEditor;
+var textEditor;
+var substitutionEditor;
+
+// TODO: block when async methods
+
+var _applause;
+
+var replace = function() {
+  var start = Date.now();
+  var sourceValue = textEditor.getValue();
+  var result = _applause.replace(sourceValue);
+  var matches = [];
+  var content = sourceValue;
+  if (result.count > 0) {
+    // highlight source from details
+    // background: rgba(113,252,159,0.8)
+    (result.detail || []).forEach(function(detail) {
+      var re = detail.match;
+      var match;
+      while ((match = re.exec(sourceValue)) !== null) {
+        var matchDetail = {
+          match: match[0],
+          start: match.index,
+          end: match.index + match[0].length - 1
+        };
+        matches.push(matchDetail);
+        // console.log('Match detail', matchDetail);
+        if (!re.global) { // or it will become infinite.
+          break;
+        }
+      }
+    });
+    content = result.content;
+  }
+  console.log('Replace matches', matches);
+  // update view
+  var count = matches.length;
+  var matchesDom = $('.options .results').removeClass('hidden');
+  if (count === 0) {
+    matchesDom.text('No matches');
+  } else if (count === 1) {
+    matchesDom.text('1 match');
+  } else {
+    matchesDom.text(count + ' matches');
+  }
+  // trace
+  console.log('Replace took ' + (Date.now() - start) + ' ms');
+  // update result
+  substitutionEditor.setValue(content, 1);
+};
+
+var create = function() {
+  var optionsValue = eval('(' + optionsEditor.getValue() + ')');
+  console.log('Create applause instance with:', optionsValue);
+  // create new applause instance
+  _applause = Applause.create($.extend({}, optionsValue, {
+    // force
+  }));
+  replace();
+};
+
+$(function() {
+  console.log('jQuery:', jQuery.fn.jquery);
+  console.log('Applause:', Applause.version);
+  console.log('CodeMirror:', CodeMirror.version);
+// update version number
+  $('header span').text('v.' + Applause.version);
+  // options
+  var optionsTextArea = document.querySelector('div.options .editor');
+  var spec = {
+    name: 'javascript',
+    json: true
+  };
+  optionsEditor = CodeMirror(optionsTextArea, {
+    value: defaultOptions,
+    lineNumbers: true,
+    mode: spec,
+    // viewportMargin: Infinity
+    autofocus: true,
+    matchBrackets: true
+  });
+  // optionsEditor.setOption('mode', spec);
+  // CodeMirror.autoLoadMode(optionsEditor, spec);
+  optionsEditor.on('change', function(codeMirror, change) {
+    // create new applause
+    create();
+    // indent on paste
+    if (change.origin !== 'paste') {
+      return;
+    }
+    var lineFrom = change.from.line;
+    var lineTo = change.from.line + change.text.length;
+    function reindentLines(codeMirror, lineFrom, lineTo) {
+      codeMirror.operation(function() {
+        codeMirror.eachLine(lineFrom, lineTo, function(lineHandle) {
+          codeMirror.indentLine(lineHandle.lineNo(), 'smart');
+        });
+      });
+    }
+    reindentLines(codeMirror, lineFrom, lineTo);
+  });
+  // text
+  var textTextArea = document.querySelector('div.text .editor');
+  textEditor = CodeMirror(textTextArea, {
+    value: defaultText,
+    mode: 'Plain Text',
+    lineNumbers: true
+  });
+  textEditor.on('change', replace);
+  // substitution
+  var substitutionTextArea = document.querySelector('div.substitution .editor');
+  substitutionEditor = CodeMirror(substitutionTextArea, {
+    mode: 'Plain Text',
+    lineNumbers: true,
+    // value: defaultOptions,
+    readOnly: true
+  });
+  // create initial instance and replace
+  create();
+});
